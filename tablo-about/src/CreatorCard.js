7ae2b125c9dbf587967e4190077e0172fadb1340
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2022-02-27 21:39:16 +0100

    main

--- tablo-about/src/CreatorCard.js
+++ tablo-about/src/CreatorCard.js
@@ -5,7 +5,7 @@ export default function CreatorCard({ creator, offsetY, index }) {
     const offset = 300
     const cardOffset = index * 500
     const positionZ = offsetY - offset - cardOffset
-    const positionY = -window.innerHeight / 2 + (offsetY - offset-cardOffset)
+    const positionY = -window.innerHeight / 2 - (offsetY - offset-cardOffset)
     const opacity = (positionZ + 150) / 100
     return (
         <div style={{ opacity, transform: `translateZ(${positionZ}px) translateY(${positionY}px)` }} className='creator__card' > {creator.name}</div>
