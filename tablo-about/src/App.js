98dc7f1c55162d16ac2f44cf15e85d55947cbee5
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2022-03-15 20:37:38 +0100

    main

--- tablo-about/src/App.js
+++ tablo-about/src/App.js
@@ -1,28 +1,25 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Scene from './Scene';
 import Nav from './Nav';
 import Background from './Background';
-import { useRef } from 'react';
 
 function App() {
   const [offsetY, setOffsetY] = useState(0)
   const handleScrollY = () => setOffsetY(window.pageYOffset)
   //Scroll
-  const [mainHeight, setHeight] = useState(0)
   useEffect(() => {
-    console.log(main)
     window.addEventListener("scroll", handleScrollY)
     return () => window.removeEventListener("scroll", handleScrollY)
   }, [])
 
-  const main = useRef()
+  const [mainRef, setMainRef] = useState()
 
   return (
     <>
       <Nav />
-      <Scene ref={main} offsetY={offsetY} />
-      <Background main={main} offsetY={offsetY} />
+      <Scene setMainRef={setMainRef} offsetY={offsetY} />
+      <Background main={mainRef} offsetY={offsetY} />
     </>
   )
 }
