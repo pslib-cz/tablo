c0a6a0c09d4c815ce7cf5b672cab458ebc3cdd8b
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2022-03-31 16:40:21 +0200

    main

--- tablo-about/src/GalleryImage.js
+++ tablo-about/src/GalleryImage.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 export default function GalleryImage({ url, name, urlMin }) {
+
+  //Jdu se zabít
+  function rickRoll() {
+    if (name == "Ondřej Svoboda - Mr. Robot") {
+      const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      window.location.href = url
+    }
+  }
   return (
-    <a className='lg__image' href={url}>
+    <a onClick={rickRoll} className='lg__image' href={url}>
       <figure>
         <img loading='lazy' src={urlMin} alt={name} />
         <figcaption>{name}</figcaption>
